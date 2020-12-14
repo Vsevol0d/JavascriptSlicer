670874afdb9daf42c6e5f0ebb6835d9f7b2c5a77
sevaproj <sevkatrc@gmail.com>
2020-12-14 23:03:56 +0200

    Temp isBinary fix added

--- STLLoader.js
+++ STLLoader.js
@@ -33,13 +33,13 @@ THREE.STLLoader.prototype.parse = function ( data ) {
 
 
 	var isBinary = function () {
-
-		var expect, face_size, n_faces, reader;
-		reader = new DataView( binData );
-		face_size = (32 / 8 * 3) + ((32 / 8 * 3) * 3) + (16 / 8);
-		n_faces = reader.getUint32(80,true);
-		expect = 80 + (32 / 8) + (n_faces * face_size);
-		return expect === reader.byteLength;
+		return false;
+		// var expect, face_size, n_faces, reader;
+		// reader = new DataView( binData );
+		// face_size = (32 / 8 * 3) + ((32 / 8 * 3) * 3) + (16 / 8);
+		// n_faces = reader.getUint32(80,true);
+		// expect = 80 + (32 / 8) + (n_faces * face_size);
+		// return expect === reader.byteLength;
 
 	};
 
